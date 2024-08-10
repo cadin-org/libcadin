e8635b19e3ca72203e6e75d7faf1f01775d1fd88
lpnh <paniguel.lpnh@gmail.com>
2024-08-10 17:50:57 -0300

    feat: improve game-screen x and y points

--- game-screen/init.lua
+++ game-screen/init.lua
@@ -5,7 +5,10 @@ local M = {}
 M.width = 800
 M.height = 600
 
-M.pos_x = window.center.x - (M.width / 2)
-M.pos_y = window.center.y - (M.height / 2)
+M.pos_x0 = window.center.x - (M.width / 2)
+M.pos_y0 = window.center.y - (M.height / 2)
+
+M.pos_x1 = M.pos_x0 + M.width
+M.pos_y1 = M.pos_y0 + M.height
 
 return M
